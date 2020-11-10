63ad586df6ff07c953ed1338ec469d6fdf7d28a3
Indika Kolamba Tantrege <ikolamba2@gmail.com>
2020-11-10 02:55:56 +0000

    Installed ejs dependancy and intialized it in my tiny app

--- express_server.js
+++ express_server.js
@@ -2,17 +2,19 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
+app.set("view engine", "ejs");
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5sk": "http://www.google.com"
 }
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
 });
 
 app.get("/urls.json", (req, res) => {
