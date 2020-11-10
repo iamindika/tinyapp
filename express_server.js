930b642c09f08f4efb50c3c0e34e009f8267f6c8
Indika Kolamba Tantrege <ikolamba2@gmail.com>
2020-11-10 19:41:13 +0000

    Saved user submission of a longURL into our urlDatabase using a randomly generated shortURL from a helper function

--- express_server.js
+++ express_server.js
@@ -37,7 +37,8 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);
+  let shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
   res.send("Ok");
 })
 
